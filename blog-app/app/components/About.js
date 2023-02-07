c3976debf03d1c23b4b6cac78976e09532663ec4
SeungOhHong <ghdtmddh@gmail.com>
2023-02-07 15:08:39 +0900

    feat: 페이지 컴포넌트로 컴포넌트에 따른 제목 변경 로직 재사용

--- blog-app/app/components/About.js
+++ blog-app/app/components/About.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Container from "./Container";
-// 컨테이너를 불러오기 해준다
+import React, { useEffect } from "react";
+import Page from "./Page";
+// 컨테이너 컴포넌트를 페이지 컴포넌트로 바꿔준다
+
+// useEffect를 이용해준다
 
 function About() {
   return (
-    // 컴포넌트를 재사용하기 위해서 className을 외울 필요없이 Container 박스로 감싸면 된다
-    <Container>
+    // 컨테이너 컴포넌트를 페이지 컴포넌트로 바꿔준다
+    <Page title="About Us">
       <h2>About Us!!!!!!</h2>
       <p className="lead text-muted">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
@@ -27,7 +29,7 @@ function About() {
         nobis provident quo dolorum sapiente temporibus facere non repellendus
         consequatur cupiditate!
       </p>
-    </Container>
+    </Page>
   );
 }
 
--- blog-app/app/components/HomeGuest.js
+++ blog-app/app/components/HomeGuest.js
@@ -1,19 +1,13 @@
-import React from "react";
-import Container from "./Container";
+import React from "react"
+import Page from "./Page"
 
 function HomeGuest() {
   return (
-    // 조건에 따라서 다른 컴포넌트가 렌더링 되도록 해준다. 이 페이지에서는 wide한 ui가 나와야한다
-    <Container wide={true}>
+    <Page title="Welcome!" wide={true}>
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
           <h1 className="display-3">Remember Writing?</h1>
-          <p className="lead text-muted">
-            Are you sick of short tweets and impersonal &ldquo;shared&rdquo;
-            posts that are reminiscent of the late 90&rsquo;s email forwards? We
-            believe getting back to actually writing is the key to enjoying the
-            internet again.
-          </p>
+          <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
           <form>
@@ -21,51 +15,28 @@ function HomeGuest() {
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
-              <input
-                id="username-register"
-                name="username"
-                className="form-control"
-                type="text"
-                placeholder="Pick a username"
-                autoComplete="off"
-              />
+              <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
-              <input
-                id="email-register"
-                name="email"
-                className="form-control"
-                type="text"
-                placeholder="you@example.com"
-                autoComplete="off"
-              />
+              <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
-              <input
-                id="password-register"
-                name="password"
-                className="form-control"
-                type="password"
-                placeholder="Create a password"
-              />
+              <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
             </div>
-            <button
-              type="submit"
-              className="py-3 mt-4 btn btn-lg btn-success btn-block"
-            >
+            <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
               Sign up for ComplexApp
             </button>
           </form>
         </div>
       </div>
-    </Container>
-  );
+    </Page>
+  )
 }
 
-export default HomeGuest;
+export default HomeGuest
