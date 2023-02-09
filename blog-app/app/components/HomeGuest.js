c91106c7af97e178ed765f2080b510f51e23fbc9
SeungOhHong <ghdtmddh@gmail.com>
2023-02-09 13:48:11 +0900

    feat: 폼 데이터를 useState 이용하여 유동적으로 업데이트하기

--- blog-app/app/components/HomeGuest.js
+++ blog-app/app/components/HomeGuest.js
@@ -1,50 +1,34 @@
-import React from "react";
+// useState를 임포트한다
+import React, { useState } from "react";
 import Page from "./Page";
-// Axios는 요청을 전송하기에 아주 편리한 라이브러리이다
 import Axios from "axios";
-
-// 사용자가 form을 제출하면 폼에 입력한 값들을 백엔드 서버로 보낸 후
-// DB에 저장에 저장한다
-
+// 이제 세 개의 state를 만들어보자
+// 1. 사용자이름 2. 이메일 3. 비밀번호
+// useState(a,b) 는 두 개의 배열을 반환한다. rest 문법을 이용해서 담아 준다
 function HomeGuest() {
-  // 브라우저의 기본 동작을 막아준다
-  // 왜냐하면 브라우저의 기본 셋팅은 폼을 제출하면 새로운 URL을 전송하고
-  // 새로운 HTML을 불러오기 때문이다
-  // 우리가 현재 해야할 일은 네트워크 요청을 백엔드 서버로 전송하는 것이다
+  // a 는 현재 값이고 b 는 값을 업데이트하기 위해서 호출할 함수이다
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   async function handleSubmit(e) {
-    // try catch로 axios 요청을 묶어줌으로써 에러를 핸들링 할 수 있도록 처리해준다
     e.preventDefault();
     try {
-      // 프론트엔드 요청 전송에는 Axios 라이브러리를 사용한다
-      // npm install axios
-      // (자바스크립트에서는 fetch를 사용)
-      // 백엔드 서버로 post 요청을 전송한다
-      // post(a,b)  두 개의 매개변수를 받는다
-      // a => 요청을 전송할 서버의 URL
-      // b => 서버로 전송할 데이터
-      await Axios.post(
-        // 또한 Axios는 비동기로 처리되기 때문에 이 요청이 서버에 언제 전송이 완료되는지
-        // 그 시점을 명확히 알 수 없다. 때문에 asyc - await 키워드로 감싸줘야한다
-        // 그래야 서버 요청이 성공했을 때에 성공 메세지가 뜨게된다
-        // a
-        "http://localhost:8080/register",
-        // b
-        {
-          username: "test2",
-          email: "test2@test.com",
-          password: "qwerty123456",
-        }
-      );
-      // 요청 성공했을 때의 메세지
+      await Axios.post("http://localhost:8080/register", {
+        // 이제 하드코딩이었던 값들을 사용자가 입력한 업데이트 된 값으로 변경해준다
+        // 모던 자바스크립트에서는 값과 프로퍼티가 동일할 경우 username: username 이라고 적을 필요가 없다
+        username,
+        email,
+        password,
+      });
       console.log("User was successfully created.");
     } catch (e) {
-      // 요청 실패했을 때의 메세지
       console.log("There was an error.");
     }
   }
-  // 이제 폼을 제출하면 DB에 제출한 값이 저장된 것을 확인할 수 있다
 
   return (
+    // 이제 3개의 필드(항목)에 onChange 이벤트 리스너를 추가해준다
     <Page title="Welcome!" wide={true}>
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
@@ -58,11 +42,15 @@ function HomeGuest() {
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
           <form onSubmit={handleSubmit}>
+            {/* 1. 유저 이름 */}
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
               <input
+                // 폼의 이 항목에 변화가 있을 경우 => setUsername함수가 실행되고
+                // 괄호 안의 값을 업데이트 시킨다.
+                onChange={(e) => setUsername(e.target.value)}
                 id="username-register"
                 name="username"
                 className="form-control"
@@ -71,11 +59,13 @@ function HomeGuest() {
                 autoComplete="off"
               />
             </div>
+            {/* 2. 유저 이메일 */}
             <div className="form-group">
               <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 id="email-register"
                 name="email"
                 className="form-control"
@@ -84,11 +74,13 @@ function HomeGuest() {
                 autoComplete="off"
               />
             </div>
+            {/* 3. 유저 비밀번호 */}
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 id="password-register"
                 name="password"
                 className="form-control"
