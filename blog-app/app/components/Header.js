98428ca019814abf3f881c7d5278419c257dbeee
SeungOhHong <ghdtmddh@gmail.com>
2023-02-09 19:14:00 +0900

    feat: 로그인 상태에 따른 컴포넌트 교체(최상위 Main컴포넌트에서 props로 상태 상속해주기)

--- blog-app/app/components/Header.js
+++ blog-app/app/components/Header.js
@@ -3,15 +3,7 @@ import { Link } from "react-router-dom";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 
-function Header() {
-  // useState에 initial value(초기값)를 줄 수 있다.
-  // 만약 로컬 스토리지에 토큰이 존재한다면 그건 True인 상태이다
-  // 없는 경우에는 False를 반환할 것이다
-  const [loggedIn, setLoggedIn] = useState(
-    Boolean(localStorage.getItem("complexappToken"))
-    // 새로고침 하더라도 로그인 상태가 유지 되는 것을 확인할 수 있다
-  );
-
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -20,10 +12,12 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        {loggedIn ? (
-          <HeaderLoggedIn setLoggedIn={setLoggedIn} />
+        {/* 이제 Header컴포넌트에서도
+         props로 접근하여 로그인 상태를 전달 받을 수 있게 되었다. */}
+        {props.loggedIn ? (
+          <HeaderLoggedIn setLoggedIn={props.setLoggedIn} />
         ) : (
-          <HeaderLoggedOut setLoggedIn={setLoggedIn} />
+          <HeaderLoggedOut setLoggedIn={props.setLoggedIn} />
         )}
       </div>
     </header>
--- blog-app/app/components/HeaderLoggedOut.js
+++ blog-app/app/components/HeaderLoggedOut.js
@@ -5,9 +5,6 @@ function HeaderLoggedOut(props) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
-  // 로그인에 성공하면 서버에서 응답으로 데이터를 보내준다
-  // 그 데이터와 브라우저의 로컬스토리지 특성을 이용해서
-  // 우리 앱이 로그인 상태를 기억할 수 있도록 만들 수 있다
   async function handleSubmit(e) {
     e.preventDefault();
     try {
@@ -16,22 +13,9 @@ function HeaderLoggedOut(props) {
         password,
       });
       if (response.data) {
-        // 서버에서 응답으로 온 response.data 객체를 브라우저 로컬스토리지에 저장한다
-        // 로그인을 하면 서버에서 세가지 값을 객체로 반환한다
-        // 1. token, 2. username, 3. avatar
-        // localStorage.setItem(a,b)
-        // a 는 우리가 저장하기를 원하는 데이터의 이름이다(아무렇게나 지어도 된다)
-        // b 는 저장하려는 데이터이다
-        localStorage.setItem(
-          // a
-          "complexappToken",
-          // b
-          response.data.token
-        );
+        localStorage.setItem("complexappToken", response.data.token);
         localStorage.setItem("complexappUsername", response.data.username);
         localStorage.setItem("complexappAvatar", response.data.avatar);
-        // 브라우저의 F12를 누르고 Application 탭으로 가보면 로컬 스토리지에 저장된다.
-        // 브라우저를 새로고침 하더라도 로컬스토리지에 있는 데이터는 소멸 되지 않는 것을 볼 수 있다
         props.setLoggedIn(true);
       } else {
         console.log("Incorrect username / password.");
